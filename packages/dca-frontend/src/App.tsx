6514760b855465f1a63829480c560eedc4e8e1a8
FedericoAmura <federicoamura@gmail.com>
2025-04-29 21:53:00 +0200

    feat: adds support for custom jwt storage solution, even async. Defaults to localstorage

--- packages/dca-frontend/src/App.tsx
+++ packages/dca-frontend/src/App.tsx
@@ -5,19 +5,48 @@ import { JwtProvider, useJwtContext } from '@lit-protocol/vincent-sdk';
 
 import '@/App.css';
 
+import Loading from '@/components/loading';
 import { APP_ID } from '@/config';
 import { Home } from '@/pages/home';
 import { Login } from '@/pages/login';
 
 function AppContent() {
-  const { authInfo } = useJwtContext();
+  const { authInfo, loading } = useJwtContext();
+
+  if (loading) {
+    return <Loading />;
+  }
 
   return authInfo ? <Home /> : <Login />;
 }
 
+// Async wrapper around localstorage to show loading state
+// In reality you would use the jwt storage solution you want. SDK defaults to localstorage if nothing is passed
+const TIME = 0;
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const asyncStorage = {
+  getItem: async (key: string) => {
+    await wait(TIME);
+    return localStorage.getItem(key);
+  },
+  setItem: async (key: string, value: string) => {
+    await wait(TIME);
+    localStorage.setItem(key, value);
+  },
+  removeItem: async (key: string) => {
+    await wait(TIME);
+    localStorage.removeItem(key);
+  },
+};
+
 function App() {
   return (
-    <JwtProvider appId={APP_ID}>
+    <JwtProvider
+      appId={APP_ID}
+      storage={asyncStorage}
+      storageKeyBuilder={(appId) => `vincentApp${appId}`}
+    >
       <AppContent />
     </JwtProvider>
   );
